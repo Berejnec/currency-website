9e993ef9066dfbde0081fa5a21268065aef494c1
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-02-13 14:15:34 +0200

    added error notification to converter component

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CurrencyService} from "../services/currency.service";
 import {NgForm} from "@angular/forms";
+import {NotificationsService} from "angular2-notifications";
+import {timeout} from "rxjs/operators";
 
 @Component({
   selector: 'app-converter',
@@ -9,13 +11,16 @@ import {NgForm} from "@angular/forms";
 })
 export class ConverterComponent implements OnInit {
   @Input()
-  enteredValue!: number;
+  enteredValue!: any;
   usdValue!: number;
   eurValue!: number;
   gbpValue!: number;
   inputValue!: number;
 
-  constructor(private currency: CurrencyService) { }
+  @Output()
+  invalid: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private currency: CurrencyService, private notificationService: NotificationsService) { }
 
   ngOnInit(): void {
     this.currency.getUsdCurrency().subscribe(data => this.usdValue = data);
@@ -30,4 +35,34 @@ export class ConverterComponent implements OnInit {
     this.currency.getEuroCurrency().subscribe(data => this.eurValue = data * auxInput);
     this.currency.getGbpCurrency().subscribe(data => this.gbpValue = data * auxInput);
   }
+
+  onInvalid() {
+    if(typeof(this.enteredValue) !== 'number') {
+      this.invalid.emit('NaN');
+    }
+  }
+
+  isNumber(value: any) {
+    return typeof value === 'number';
+  }
+
+  onInvalidInput() {
+    if(!this.isNumber(this.enteredValue)) {
+      this.invalid.emit('NaN');
+    }
+  }
+
+  createNotification(input: any, form: NgForm) {
+    if(isNaN(input)) {
+      this.notificationService.error('Invalid input!', 'Enter a valid number you want to convert', {
+        timeOut: 5000,
+        showProgressBar: true,
+        pauseOnHover: true,
+        clickToClose: true,
+        animate: 'rotate'
+      });
+      form.resetForm();
+    }
+  }
+
 }
