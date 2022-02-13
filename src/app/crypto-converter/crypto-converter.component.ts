8ccf6cfc8ba5c3676b4f19747e4ce739f2042629
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-02-13 14:33:14 +0200

    added error notification for crypto-converter component

--- src/app/crypto-converter/crypto-converter.component.ts
+++ src/app/crypto-converter/crypto-converter.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CryptoService} from "../services/crypto.service";
 import {NgForm} from "@angular/forms";
+import {NotificationsService} from "angular2-notifications";
 
 @Component({
   selector: 'app-crypto-converter',
@@ -13,15 +14,28 @@ export class CryptoConverterComponent implements OnInit {
   cryptoValue!: number;
   btcUsdValue!: number;
 
-  constructor(private crypto: CryptoService) { }
+  constructor(private crypto: CryptoService, private notificationService: NotificationsService) { }
 
   ngOnInit(): void {
     this.crypto.getBTCUSD().subscribe(price => this.cryptoValue = price);
   }
 
-  onSubmit(form: NgForm) {
+  onSubmit() {
     let aux = this.inputValue;
     this.crypto.getBTCUSD().subscribe(price => this.btcUsdValue = aux * price);
   }
 
+  createNotification(input: any, form: NgForm) {
+    if(isNaN(input)) {
+      this.notificationService.error('Invalid input!', 'Enter a valid number of Bitcoins you want to convert', {
+        timeOut: 3500,
+        showProgressBar: true,
+        pauseOnHover: true,
+        clickToClose: true,
+        animate: 'fromRight',
+        preventDuplicates: true
+      });
+      form.resetForm();
+    }
+  }
 }
